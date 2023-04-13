e5841c92dcb33e22e4fc95006d467fa5e03b03d0
PARKPARKWOO <wy9295@naver.com>
2023-04-13 23:44:10 +0900

    pzl

--- src/test/java/com/example/demo/DemoApplicationTests.java
+++ src/test/java/com/example/demo/DemoApplicationTests.java
@@ -5,7 +5,6 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-@TestPropertySource(properties = { "spring.config.location=classpath:application-test.yml" })
 class DemoApplicationTests {
 
     @Test
